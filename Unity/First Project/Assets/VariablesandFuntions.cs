88364aa57f116a48721561b8377d6da08e251815
Benjamin Ricks <Fuzzinator12@gmail.com>
2015-09-01 09:51:04 -0600

    Variables and functions

--- Unity/First Project/Assets/VariablesandFuntions.cs
+++ Unity/First Project/Assets/VariablesandFuntions.cs
@@ -3,13 +3,16 @@ using System.Collections;
 
 public class VariablesandFuntions : MonoBehaviour {
 
+	int Number= 5;
+
 	// Use this for initialization
 	void Start () {
-	
+		Number = Multiply (Number);
+		Debug.Log (Number);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	int Multiply (int Math){
+		int ret;
+		ret = Math * 3;
+		return ret;
 	}
 }
