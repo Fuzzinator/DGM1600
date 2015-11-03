715bc2de4b5a427d95bd57de1fe39f07e6a88d27
Benjamin Ricks <Fuzzinator12@gmail.com>
2015-11-03 11:59:13 -0700

    trying to catch up with my schoolwork...

---
+++ Unity/First Project/Assets/Scopes_and_Modifiers.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scopes_and_Modifiers : MonoBehaviour {
+
+	public int respawn = 1;
+
+	private int two = 20;
+
+	private int three = 15;
+
+	private ClassTwo foodClass;
+
+
+
+	// Use this for initialization
+	void Start () {
+		respawn = 6;
+		foodClass = new ClassTwo ();
+		foodClass.Zambaroonies(respawn, foodClass.emptyCan);
+		while (respawn <= 6 && respawn != 0) {
+			respawn --;
+
+		}
+	}
+
+	void Example (int walkers, int runners){
+		int totalZambos;
+		totalZambos = (walkers + runners) * respawn;
+		Debug.Log (totalZambos);
+	}
+
+	// Update is called once per frame
+	void Update () {
+			while(respawn != 0) {
+			Debug.Log ("Zambaroonies! There are:" + respawn);
+		}
+	}
+}
